b38791862fb23dec5daaf788e1fdd43dfec9c43d
Maria Kornaukhova <kornaukhova.masha@lit1533.ru>
2024-01-24 19:57:53 +0300

    My first commit

---
+++ Array1.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ConsoleApp1;
+
+
+namespace ConsoleApp1
+{
+    public class Array1
+    {
+        private int[] array;
+
+        public Array1(int length, bool usInp = false)
+        {
+            array = new int[length];
+
+            if (usInp)
+            {
+
+                ArrUsInp();
+            }
+
+            else
+            {
+                ArrRand();
+            }
+        }
+
+        public void ArrUsInp()
+        {
+            Console.WriteLine($"Введите {array.Length} чисел:");
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = int.Parse(Console.ReadLine());
+            }
+        }
+
+        public void ArrRand()
+        {
+            Random random = new Random();
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = random.Next(1, 101);
+            }
+
+            Console.WriteLine("Рандомные значаения: ");
+        }
+
+        public double Average()
+        {
+            int sum = 0;
+            foreach (int value in array)
+            {
+                sum += value;
+            }
+            return (double)sum / array.Length;
+        }
+
+
+        public void RemoveElements()
+        {
+            int[] newArray = new int[array.Length];
+            int j = 0;
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (Math.Abs(array[i]) <= 100)
+                {
+                    newArray[j] = array[i];
+                    j++;
+                }
+            }
+            Array.Resize(ref newArray, j);
+            array = newArray;
+        }
+
+
+
+        public void RemoveDuplicates()
+        {
+            int count = 0;
+            for (int i = 0; i < array.Length; i++)
+            {
+                bool isDuplicate = false;
+                for (int j = 0; j < i; j++)
+                {
+                    if (array[i] == array[j])
+                    {
+                        isDuplicate = true;
+                        break;
+                    }
+                }
+                if (!isDuplicate)
+                {
+                    count++;
+                }
+            }
+
+            int[] result = new int[count];
+            count = 0;
+            for (int i = 0; i < array.Length; i++)
+            {
+                bool isDuplicate = false;
+                for (int j = 0; j < i; j++)
+                {
+                    if (array[i] == array[j])
+                    {
+                        isDuplicate = true;
+                        break;
+                    }
+                }
+                if (!isDuplicate)
+                {
+                    result[count] = array[i];
+                    count++;
+                }
+            }
+            array = result;
+        }
+
+
+        public void PrintArray()
+        {
+            Console.WriteLine("Элементы массива:");
+            foreach (int value in array)
+            {
+                Console.Write(value + " ");
+            }
+            Console.WriteLine();
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,95 @@
+﻿using ConsoleApp1;
+using Test;
+
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Array1 randArr = new Array1(3, false);
+        randArr.PrintArray();
+        Console.WriteLine("Среднее значение: " + randArr.Average());
+
+        // Создание экземпляра класса с заполнением значениями, введенными пользователем
+        Array1 usInpArr = new Array1(3, true);
+        usInpArr.PrintArray();
+        Console.WriteLine("Среднее значение: " + usInpArr.Average());
+
+        Array1 arrRemover = new Array1(3, true);
+        arrRemover.PrintArray();
+        arrRemover.RemoveElements();
+        Console.WriteLine("Mассив, где удалены все элементы, большие 100 по модулю: ");
+        arrRemover.PrintArray();
+
+        Array1 arrRemoveDupl = new Array1(3, true);
+        arrRemoveDupl.PrintArray();
+        Console.WriteLine("Массив, где удалены все повторяющиеся элементы: ");
+        arrRemoveDupl.RemoveDuplicates();
+        arrRemoveDupl.PrintArray();
+
+
+
+        Console.WriteLine("Вводим размеры:");
+        int rows = int.Parse(Console.ReadLine());
+        int columns = int.Parse(Console.ReadLine());
+
+        Array2 array1 = new Array2(rows, columns);
+        Console.WriteLine("Массив1:");
+        array1.PrintArray2();
+        Console.WriteLine($"Среднее: {array1.Average()}");
+        Console.WriteLine($"Массив1 с четными строками в обратном порядке:");
+        array1.InReverse();
+
+        Array2 array2 = new Array2(rows, columns, true);
+        Console.WriteLine("Массив2:");
+        array2.PrintArray2();
+        Console.WriteLine($"Среднее: {array2.Average()}");
+        Console.WriteLine($"Массив2 с четными строками в обратном порядке:");
+        array2.InReverse();
+
+
+
+
+        Console.Write("Введите количество строк в ступенчатом массиве: ");
+
+        int[] values = new int[rows];
+
+        for (int i = 0; i < rows; i++)
+        {
+            Console.Write($"Введите количество значений в строке {i + 1}: ");
+            values[i] = int.Parse(Console.ReadLine());
+        }
+
+        Array3 randArr = new Array3(rows, values);
+
+        Console.WriteLine("Ступенчатый массив с рандомными значениями:");
+        randArr.PrintArray();
+
+        Console.WriteLine($"Среднее: {randArr.Average()}");
+        randArr.ModifyArray();
+
+        foreach (double i in randArr.GetAverages())
+        {
+            Console.WriteLine($"Среднее для вложенного массива: {i}");
+        }
+
+        Console.WriteLine("Измененный: ");
+        randArr.PrintArray();
+
+        Array3 userArr = new Array3(rows);
+
+        Console.WriteLine("\nСтупенчатый массив с вводом значений пользователем:");
+        userArr.PrintArray();
+
+        Console.WriteLine($"Среднее: {userArr.Average()}");
+
+        foreach (double i in userArr.GetAverages())
+        {
+            Console.WriteLine($"Среднее для вложенного массива: {i}");
+        }
+
+        Console.WriteLine("Измененный: ");
+        userArr.ModifyArray();
+        userArr.PrintArray();
+    }
+}
